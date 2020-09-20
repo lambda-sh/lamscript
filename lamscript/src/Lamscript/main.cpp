472f63f66bd07e0ccf24035fb328d40ab18fa271
C3NZ <cenz@cenz.io>
2020-09-19 19:22:12 -0700

    [fix] formatting for output.

--- lamscript/src/Lamscript/main.cpp
+++ lamscript/src/Lamscript/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
     std::cout << "Usage: lamscript [script]" << std::endl; 
     exit(64);
   } else if (argc == 2) {
-    std::cout << "This would run the script" << argv[1] << std::endl;
+    std::cout << "This would run the script: " << argv[1] << std::endl;
     exit(0);
   } else {
     std::cout << "This would be interpreted." << std::endl;
