006a3092c877b315659565a986dfa10c2cad40bd
C3NZ <cenz@cenz.io>
2020-09-29 20:41:24 -0700

    [add] a function to convert lamscript tokens into strings for representation in Tokens when they're converted to strings.

--- src/Lamscript/TokenType.h
+++ src/Lamscript/TokenType.h
@@ -1,5 +1,8 @@
-#ifndef LAMSCRIPT_SRC_LAMSCRIPT_TOKENTYPE_H_
-#define LAMSCRIPT_SRC_LAMSCRIPT_TOKENTYPE_H_
+#ifndef SRC_LAMSCRIPT_TOKENTYPE_H_
+#define SRC_LAMSCRIPT_TOKENTYPE_H_
+
+#include <string>
+
 namespace lamscript {
 
 /// @brief Tokens supported by the lamscript parser.
@@ -9,19 +12,66 @@ enum TokenType {
   SEMICOLON, SLASH, STAR,
 
   // boolean operation tokens
-  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, 
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS,
   LESS_EQUAL,
 
   // Literals
   IDENTIFIER, STRING, NUMBER,
 
   // Keywords
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, 
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS,
   TRUE, VAR, WHILE,
 
   // etc
-  EOF
+  END_OF_FILE
 };
 
+/// @brief Convert TokenType enum values into their corresponding string
+/// representations.
+std::string ConvertTokenTypeToString(TokenType token_type) {
+  switch (token_type) {
+    case LEFT_PAREN:    return "(";
+    case RIGHT_PAREN:   return ")";
+    case LEFT_BRACE:    return "[";
+    case RIGHT_BRACE:   return "]";
+    case COMMA:         return ",";
+    case DOT:           return ".";
+    case MINUS:         return "-";
+    case PLUS:          return "+";
+    case SEMICOLON:     return ";";
+    case SLASH:         return "/";
+    case STAR:          return "*";
+    case BANG:          return "!";
+    case BANG_EQUAL:    return "!=";
+    case EQUAL:         return "=";
+    case EQUAL_EQUAL:   return "==";
+    case GREATER:       return ">";
+    case GREATER_EQUAL: return ">=";
+    case LESS:          return "<";
+    case LESS_EQUAL:    return "<=";
+    case IDENTIFIER:    return "Identifier";
+    case STRING:        return "String";
+    case NUMBER:        return "Number";
+    case AND:           return "&";
+    case CLASS:         return "class";
+    case ELSE:          return "else";
+    case FALSE:         return "False";
+    case TRUE:          return "true";
+    case FUN:           return "fun";
+    case FOR:           return "for";
+    case IF:            return "if";
+    case NIL:           return "nil";
+    case OR:            return "print";
+    case RETURN:        return "return";
+    case SUPER:         return "super";
+    case THIS:          return "this";
+    case VAR:           return "var";
+    case WHILE:         return "while";
+    case END_OF_FILE:   return "eof";
+    default:            return "";
+  }
 }
-#endif  // LAMSCRIPT_SRC_LAMSCRIPT_TOKENTYPE_H_
+
+}  // namespace lamscript
+
+#endif  // SRC_LAMSCRIPT_TOKENTYPE_H_
