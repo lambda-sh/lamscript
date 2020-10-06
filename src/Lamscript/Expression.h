ec750997baa4c72a82f2654f3d54d564ed520b2e
C3NZ <cenz@cenz.io>
2020-10-06 14:48:09 -0700

    [update] expressions to use the visitor design pattern for using one interface to perform evaluations on expressions while maintaining ease of scalability if the language chooses to support more.

--- src/Lamscript/Expression.h
+++ src/Lamscript/Expression.h
@@ -7,6 +7,10 @@
 
 namespace lamscript {
 
+// Forward declaration for the Visitor interface.
+template<typename ReturnType>
+class Visitor;
+
 /// @brief Base expression class used for allowing expressions to work with one
 /// another
 class Expression {};
@@ -17,6 +21,12 @@ class Binary : Expression {
   Binary(Expression* left, Token expression_operator, Expression* right)
       : left_(left), operator_(expression_operator), right_(right) {}
 
+
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitBinaryExpression(this);
+  }
+
  private:
   Expression* left_;
   Token operator_;
@@ -26,6 +36,11 @@ class Binary : Expression {
 class Assign : Expression {
  public:
   Assign(Token name, Expression* value) : name_(name), value_(value) {}
+
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitAssignExpression(this);
+  }
 
  private:
   Token name_;
@@ -40,6 +55,11 @@ class Call : Expression {
       std::vector<Expression*> arguments)
           : callee_(callee), parentheses_(parentheses), arguments_(arguments) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitCallExpression(this);
+  }
+
  private:
   Expression* callee_;
   Token parentheses_;
@@ -51,6 +71,11 @@ class Get : Expression {
  public:
   Get(Expression* object, Token name) : object_(object), name_(name) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitGetExpression(this);
+  }
+
  private:
   Expression* object_;
   Token name_;
@@ -60,6 +85,11 @@ class Grouping : Expression {
  public:
   explicit Grouping(Expression* expression) : expression_(expression) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitGroupingExpression(this);
+  }
+
  private:
   Expression* expression_;
 };
@@ -67,6 +97,11 @@ class Grouping : Expression {
 class Literal : Expression {
  public:
   explicit Literal(void* value) : value_(value) {}
+
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitLiteralExpression(this);
+  }
 
  private:
   void* value_;
@@ -76,6 +111,11 @@ class Logical : Expression {
  public:
   Logical(Expression* left, Token logical_operator, Expression* right)
       : left_(left), logical_operator_(logical_operator), right_(right) {}
+
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitLogicalExpression(this);
+  }
 
  private:
   Expression* left_;
@@ -88,6 +128,11 @@ class Set : Expression {
   Set(Expression* object, Token name, Expression* value)
       : object_(object), name_(name), value_(value) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitSetExpression(this);
+  }
+
  private:
   Expression* object_;
   Token name_;
@@ -98,6 +143,11 @@ class Super : Expression {
  public:
   Super(Token keyword, Token method) : keyword_(keyword), method_(method) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitSuperExpression(this);
+  }
+
  private:
   Token keyword_;
   Token method_;
@@ -106,6 +156,11 @@ class Super : Expression {
 class This : Expression {
  public:
   explicit This(Token keyword) : keyword_(keyword) {}
+
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitThisExpression(this);
+  }
 
  private:
   Token keyword_;
@@ -116,6 +171,11 @@ class Unary : Expression {
   Unary(Token unary_operator, Expression* right)
       : unary_operator_(unary_operator), right_(right) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitUnaryExpression(this);
+  }
+
  private:
   Token unary_operator_;
   Expression* right_;
@@ -125,8 +185,31 @@ class Variable : Expression {
  public:
   explicit Variable(Token name) : name_(name) {}
 
+  template<typename ReturnType>
+  ReturnType Accept(Visitor<ReturnType> visitor) {
+    visitor.VisitVariableExpression(this);
+  }
+
  private:
   Token name_;
+};
+
+template<typename ReturnType>
+class Visitor {
+ protected:
+  friend class Expression;
+  ReturnType VisitAssignExpression(Assign expression);
+  ReturnType VisitBinaryExpression(Binary expression);
+  ReturnType VisitCallExpression(Call expression);
+  ReturnType VisitGetExpression(Get expression);
+  ReturnType VisitGroupingExpression(Grouping expression);
+  ReturnType VisitLiteralExpression(Literal expression);
+  ReturnType VisitLogicalExpression(Logical expression);
+  ReturnType VisitSetExpression(Set expression);
+  ReturnType VisitSuperExpression(Super expression);
+  ReturnType VisitThisExpression(This expression);
+  ReturnType VisitUnaryExpression(Unary expression);
+  ReturnType VisitVariableExpression(Variable expression);
 };
 
 }  // namespace lamscript
