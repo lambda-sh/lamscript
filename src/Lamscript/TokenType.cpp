b5637cc5ca704a48090b68b8321878666f9c4ac1
C3NZ <cenz@cenz.io>
2020-10-07 18:56:33 -0700

    [update] the function definition of ConvertTokenTypeToString to be placed inside of the header file with the implementation inside of it's own cpp file.

---
+++ src/Lamscript/TokenType.cpp
@@ -0,0 +1,51 @@
+#include <Lamscript/TokenType.h>
+
+namespace lamscript {
+
+/// @brief Convert TokenType enum values into their corresponding string
+/// representations.
+std::string ConvertTokenTypeToString(TokenType token_type) {
+  switch (token_type) {
+    case LEFT_PAREN:    return "(";
+    case RIGHT_PAREN:   return ")";
+    case LEFT_BRACE:    return "[";
+    case RIGHT_BRACE:   return "]";
+    case COMMA:         return ",";
+    case DOT:           return ".";
+    case MINUS:         return "-";
+    case PLUS:          return "+";
+    case SEMICOLON:     return ";";
+    case SLASH:         return "/";
+    case STAR:          return "*";
+    case BANG:          return "!";
+    case BANG_EQUAL:    return "!=";
+    case EQUAL:         return "=";
+    case EQUAL_EQUAL:   return "==";
+    case GREATER:       return ">";
+    case GREATER_EQUAL: return ">=";
+    case LESS:          return "<";
+    case LESS_EQUAL:    return "<=";
+    case IDENTIFIER:    return "Identifier";
+    case STRING:        return "String";
+    case NUMBER:        return "Number";
+    case AND:           return "&";
+    case CLASS:         return "class";
+    case ELSE:          return "else";
+    case FALSE:         return "False";
+    case TRUE:          return "true";
+    case FUN:           return "fun";
+    case FOR:           return "for";
+    case IF:            return "if";
+    case NIL:           return "nil";
+    case OR:            return "print";
+    case RETURN:        return "return";
+    case SUPER:         return "super";
+    case THIS:          return "this";
+    case VAR:           return "var";
+    case WHILE:         return "while";
+    case END_OF_FILE:   return "eof";
+    default:            return "";
+  }
+}
+
+}  // namespace lamscript
