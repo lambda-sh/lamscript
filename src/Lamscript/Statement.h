17d2afc61c4fd1448e57f2f8565284f3093c4fb5
C3NZ <cenz@cenz.io>
2020-10-29 12:09:53 -0700

    [add] the statement abstract class.

--- src/Lamscript/Statement.h
+++ src/Lamscript/Statement.h
@@ -1,7 +1,15 @@
 #ifndef SRC_LAMSCRIPT_STATEMENT_H_
 #define SRC_LAMSCRIPT_STATEMENT_H_
 
+#include <any>
+
 namespace lamscript {
+
+class StatementVisitor;
+
+class Statement {
+  virtual std::any Accept(StatementVisitor) = 0;
+};
 
 }  // namespace lamscript
 
