bf4e2c7954f20e46898feae1d9cc0cdb6d4637ac
C3NZ <cenz@cenz.io>
2020-10-29 12:06:10 -0700

    [update] the visitor pattern to have interfaces for both expressions and statements.

--- src/Lamscript/Visitor.h
+++ src/Lamscript/Visitor.h
@@ -6,7 +6,7 @@
 namespace lamscript {
 
 /// @brief The visitor interface that allows for expressions
-class Visitor {
+class ExpressionVisitor {
  public:
   virtual std::any VisitAssignExpression(Assign* expression) = 0;
   virtual std::any VisitBinaryExpression(Binary* expression) = 0;
@@ -20,6 +20,20 @@ class Visitor {
   virtual std::any VisitThisExpression(This* expression) = 0;
   virtual std::any VisitUnaryExpression(Unary* expression) = 0;
   virtual std::any VisitVariableExpression(Variable* expression) = 0;
+};
+
+/// @brief The visitor interface for evaluating statements.
+class StatementVisitor {
+ public:
+  virtual std::any VisitBlockStatement() = 0;
+  virtual std::any VisitClassStatement() = 0;
+  virtual std::any VisitExpressionStatement() = 0;
+  virtual std::any VisitFunctionStatement() = 0;
+  virtual std::any VisitIfStatement() = 0;
+  virtual std::any VisitPrintStatement() = 0;
+  virtual std::any VisitReturnStatement() = 0;
+  virtual std::any VisitVarStatement() = 0;
+  virtual std::any VisitWhileStatement() = 0;
 };
 
 
