4f065238b3cb6ff907183b67c2a7bb30bbc6baaf
C3NZ <cenz@cenz.io>
2020-11-08 11:29:14 -0800

    [remove] all the extra baggage from testing and allow the lamscript runtime to execute as expected.

--- src/Lamscript/Main.cpp
+++ src/Lamscript/Main.cpp
@@ -10,25 +10,10 @@
 #include <Lamscript/TokenType.h>
 
 int main(int argc, char** argv) {
-  lamscript::Expression* expression = new lamscript::Binary(
-      new lamscript::Unary(
-          lamscript::Token{lamscript::MINUS, "-", nullptr, 1},
-          new lamscript::Literal(123)),
-      lamscript::Token{lamscript::STAR, "*", nullptr, 1},
-      new lamscript::Grouping(
-          new lamscript::Literal(45.67)));
-
-  std::cout
-      << std::any_cast<std::string>(
-          (new lamscript::AstPrinter())->Print(expression));
-
-  exit(0);
-
   if (argc > 2) {
     std::cout << "Usage: lamscript [script]" << std::endl;
     exit(64);
   } else if (argc == 2) {
-    std::cout << "This would run the script: " << argv[1] << std::endl;
     lamscript::Lamscript::RunFile(argv[1]);
     exit(0);
   } else {
