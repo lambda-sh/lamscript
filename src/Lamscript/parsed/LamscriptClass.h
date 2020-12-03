4e65753f4c585fdf133e000cd1df332502bb9a3c
cenz <cenz@cenz.io>
2020-12-03 11:01:27 -0800

    [update] Lamscript instances to enable shared from this such that instances can bind shared_ptrs to lamscript functions, allowing get and set methods in the interpreter to properly occur with no additional handling.

--- src/Lamscript/parsed/LamscriptClass.h
+++ src/Lamscript/parsed/LamscriptClass.h
@@ -15,7 +15,7 @@ class LamscriptClass : public LamscriptCallable {
  public:
   LamscriptClass(
       const std::string& name,
-      std::unordered_map<std::string, parsed::LamscriptFunction*> methods)
+      std::unordered_map<std::string, LamscriptFunction>&& methods)
               : name_(name), methods_(methods) {}
 
   int Arity() const override { return 0; }
@@ -30,50 +30,53 @@ class LamscriptClass : public LamscriptCallable {
 
   /// @brief Looks up a method inside of the current class definition. If it
   /// doesn't exist, returns a nullptr.
-  parsed::LamscriptFunction* LookupMethod(std::string method_name) {
-    if (methods_.contains(method_name)) {
-      return methods_.at(method_name);
+  LamscriptFunction& LookupMethod(const std::string& method_name) {
+    std::cout << "looking up method by name: " << method_name << std::endl;
+    for (auto& method : methods_) {
+      std::cout << method.first << std::endl;
     }
-
-    return nullptr;
+    return methods_.at(method_name);
   }
 
   std::string ToString() const override { return name_; }
 
  private:
   std::string name_;
-  std::unordered_map<std::string, parsed::LamscriptFunction*> methods_;
+  std::unordered_map<std::string, parsed::LamscriptFunction> methods_;
 };
 
 
 /// @brief Instance of a lamscript class.
-class LamscriptInstance {
+class LamscriptInstance :
+    public std::enable_shared_from_this<LamscriptInstance> {
  public:
   explicit LamscriptInstance(
       LamscriptClass* class_def) : class_def_(class_def) {}
 
   std::any GetField(const parsing::Token& name) {
-    if (fields.contains(name.Lexeme)) {
-      return fields.at(name.Lexeme);
+    if (fields_.contains(name.Lexeme)) {
+      return fields_.at(name.Lexeme);
     }
 
-    LamscriptCallable* method = class_def_->LookupMethod(name.Lexeme);
-    if (method != nullptr) {
-      return method;
+    // Binds the function to the current instance, allowing the use of `this`
+    // to correctly be resolved.
+    try {
+      LamscriptFunction& method = class_def_->LookupMethod(name.Lexeme);
+      return method.Bind(shared_from_this());
+    } catch (const std::out_of_range& err) {
+      throw RuntimeError(name, "Undefined property '" + name.Lexeme + "'.");
     }
-
-    throw RuntimeError(name, "Undefined property '" + name.Lexeme + "'.");
   }
 
   void SetField(const parsing::Token& name, std::any value) {
-    fields[name.Lexeme] = value;
+    fields_[name.Lexeme] = value;
   }
 
   std::string ToString() const { return class_def_->ToString() + " Instance"; }
 
  private:
   LamscriptClass* class_def_;
-  std::unordered_map<std::string, std::any> fields;
+  std::unordered_map<std::string, std::any> fields_;
 };
 
 }  // namespace parsed
