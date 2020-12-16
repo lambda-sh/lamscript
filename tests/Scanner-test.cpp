54d94622ee6a579289c757ae3da54550d30481bc
cenz <cenz@cenz.io>
2020-12-16 15:48:49 -0800

    [update] Scanner testing to include another test for print statements.

--- tests/Scanner-test.cpp
+++ tests/Scanner-test.cpp
@@ -1,8 +1,23 @@
 #include "gtest/gtest.h"
 
 #include <Lamscript/parsing/Scanner.h>
+#include <Lamscript/parsing/Token.h>
+
+using lamscript::parsing::Scanner;
+using lamscript::parsing::Token;
 
 TEST(Scanner, SuccessfullyParseTokens) {
-  lamscript::parsing::Scanner scanner("print \"Hello, world!\"");
+  Scanner scanner("print \"Hello, world!\"");
   scanner.ScanTokens();
+}
+
+TEST(Scanner, SuccessfullyParsePrint) {
+  Scanner scanner("print \"Hello, world!\"");
+  const std::vector<Token> tokens = scanner.ScanTokens();
+
+  const Token& print = tokens[0];
+  ASSERT_EQ(print.Lexeme, "print");
+
+  const Token& literal = tokens[1];
+  ASSERT_EQ(literal.Lexeme, "\"Hello, world!\"");
 }
