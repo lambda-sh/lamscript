43f791f423cee3a2be66d9dd4e5f360cac183a83
cenz <cenz@cenz.io>
2020-12-16 16:04:15 -0800

    [update] setup script to be used within a CI environment.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,22 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 pushd $ROOT_DIR > /dev/null
 
+
 # Fetch submodules.
-git submodule update --init
+git submodule update --init --recursive
 
 # Source lambda-sh for our bash scripts.
 source lambda-sh/lambda.sh
 
 LAMBDA_INFO "Successfully installed submodules and setup lambda.sh"
+
+LAMBDA_PARSE_ARG within-ci false \
+    "Used when setup is being done within a CI system."
+LAMBDA_COMPILE_ARGS $@
+
+if [ "$LAMBDA_within_ci" = true ]; then
+    exit
+fi
 
 if command -v pre-commit > /dev/null; then
     pre-commit install > /dev/null
