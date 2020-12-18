e3351c6041e113fd1da18ee2e85b2682ee6d2ff8
cenz <cenz@cenz.io>
2020-12-17 19:33:53 -0800

    [add] MSBuild to the workflow and also add an os flag for windows CI to use for MSBuild.

--- scripts/compile_lamscript.sh
+++ scripts/compile_lamscript.sh
@@ -11,6 +11,7 @@ source lambda-sh/lambda.sh
 LAMBDA_PARSE_ARG build Release "The type of build to produce."
 LAMBDA_PARSE_ARG cpp-compiler g++ "The compiler to use for C++ code."
 LAMBDA_PARSE_ARG cores 6 "The number of cores used for compilation."
+LAMBDA_PARSE_ARG os Linux "The operating system being built on."
 
 LAMBDA_COMPILE_ARGS $@
 
@@ -41,10 +42,12 @@ LAMBDA_ASSERT_LAST_COMMAND_OK \
 
 
 # ----------------------------------- BUILD ------------------------------------
-pwd
-ls
 
-make -j $LAMBDA_cores
+if [ $LAMBDA_os = "Linux" ] || [ $LAMBDA_os = "Macos" ]; then
+    make -j $LAMBDA_cores
+elif [ $LAMBDA_os = "Windows" ]; then
+    MSBuild.exe "lamscript.sln" \/t:Rebuild \/p:Configuration=$LAMBDA_build
+fi
 
 LAMBDA_ASSERT_LAST_COMMAND_OK "Couldn't successfully compile lamscript."
 
@@ -52,4 +55,4 @@ popd > /dev/null  # build
 
 LAMBDA_INFO "Successfully compiled lamscript."
 
-$popd  # ROOT_DIR
+popd > /dev/null  # ROOT_DIR
