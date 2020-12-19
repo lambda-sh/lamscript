33a733404afd05dce604581f137fe0b30047e792
cenz <cenz@cenz.io>
2020-12-19 09:34:16 -0800

    [update] tests to include parsing for END_OF_FILE tokens at the end of the token array.

--- tests/Scanner-test.cpp
+++ tests/Scanner-test.cpp
@@ -5,6 +5,7 @@
 
 using lamscript::parsing::Scanner;
 using lamscript::parsing::Token;
+using lamscript::parsing::TokenType;
 
 TEST(Scanner, SuccessfullyParseTokens) {
   Scanner scanner("print \"Hello, world!\"");
@@ -15,9 +16,16 @@ TEST(Scanner, SuccessfullyParsePrint) {
   Scanner scanner("print \"Hello, world!\"");
   const std::vector<Token> tokens = scanner.ScanTokens();
 
+  ASSERT_EQ(tokens.size(), 3);
+
   const Token& print = tokens[0];
   ASSERT_EQ(print.Lexeme, "print");
 
   const Token& literal = tokens[1];
   ASSERT_EQ(literal.Lexeme, "\"Hello, world!\"");
+
+  const Token& eof = tokens[2];
+  ASSERT_EQ(eof.Type, TokenType::END_OF_FILE);
 }
+
+TEST(Scanner, SuccessfullyParse) {}
