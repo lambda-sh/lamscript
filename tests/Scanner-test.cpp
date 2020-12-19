8a7658cd000e11a93901f520599c71857ba478c0
cenz <cenz@cenz.io>
2020-12-19 09:48:57 -0800

    [update] the names of the tests and also include semicolons in the scanning.

--- tests/Scanner-test.cpp
+++ tests/Scanner-test.cpp
@@ -7,16 +7,30 @@ using lamscript::parsing::Scanner;
 using lamscript::parsing::Token;
 using lamscript::parsing::TokenType;
 
-TEST(Scanner, SuccessfullyParseTokens) {
+TEST(Scanner, SuccessfullyScanAnything) {
   Scanner scanner("print \"Hello, world!\"");
   scanner.ScanTokens();
 }
 
-TEST(Scanner, SuccessfullyParsePrint) {
-  Scanner scanner("print \"Hello, world!\"");
+TEST(Scanner, SuccessfullyScanNumber) {
+  Scanner scanner("34;");
   const std::vector<Token> tokens = scanner.ScanTokens();
-
   ASSERT_EQ(tokens.size(), 3);
+
+  const Token& number = tokens[0];
+  ASSERT_EQ(number.Lexeme, "34");
+
+  const Token& semicolon = tokens[1];
+  ASSERT_EQ(semicolon.Lexeme, ";");
+
+  const Token& eof = tokens[2];
+  ASSERT_EQ(eof.Type, TokenType::END_OF_FILE);
+}
+
+TEST(Scanner, SuccessfullyScanPrint) {
+  Scanner scanner("print \"Hello, world!\";");
+  const std::vector<Token> tokens = scanner.ScanTokens();
+  ASSERT_EQ(tokens.size(), 4);
 
   const Token& print = tokens[0];
   ASSERT_EQ(print.Lexeme, "print");
@@ -24,8 +38,9 @@ TEST(Scanner, SuccessfullyParsePrint) {
   const Token& literal = tokens[1];
   ASSERT_EQ(literal.Lexeme, "\"Hello, world!\"");
 
-  const Token& eof = tokens[2];
+  const Token& semicolon = tokens[2];
+  ASSERT_EQ(semicolon.Lexeme, ";");
+
+  const Token& eof = tokens[3];
   ASSERT_EQ(eof.Type, TokenType::END_OF_FILE);
 }
-
-TEST(Scanner, SuccessfullyParse) {}
