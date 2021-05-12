f75d8ea5da723b7e0b41ac55089fcbeecfa8bf08
C3NZ <cenz@cenz.io>
2021-05-11 20:44:10 -0700

    [fix] test runner for windows.

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -8,8 +8,6 @@ pushd $ROOT_DIR > /dev/null
 # ----------------------------- LAMBDA-SH & ARGS ------------------------------
 source lambda-sh/lambda.sh
 
-LAMBDA_PARSE_ARG build Release "The type of build to run tests for."
-
 LAMBDA_PARSE_ARG os Linux "The operating system being built on."
 
 LAMBDA_COMPILE_ARGS $@
@@ -23,7 +21,7 @@ if [ $LAMBDA_os = "Linux" ] || [ $LAMBDA_os = "Macos" ]; then
     cp -r "$ROOT_DIR/examples" examples
     ./lamscript_tests
 elif [ "$LAMBDA_os" = "Windows" ]; then
-    pushd "$ROOT_DIR/build/bin/$LAMBDA_build" > /dev/null
+    pushd "$ROOT_DIR/build/bin" > /dev/null
     cp -r "$ROOT_DIR/examples/" examples
     ./lamscript_tests.exe
 else
@@ -32,5 +30,5 @@ fi
 
 LAMBDA_INFO "Finished executing $LAMBDA_example"
 
-popd > /dev/null  # $ROOT_DIR/build/$LAMBDA_build/bin
+popd > /dev/null  # $ROOT_DIR/build/bin
 popd > /dev/null  # $ROOT_DIR
